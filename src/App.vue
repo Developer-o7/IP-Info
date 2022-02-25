2938ff8bbbf66e1ef02e49c8159a5b7716d58187
Muzaffar <muzaffarmasordinov@gmail.com>
2022-02-25 12:18:15 +0500

    Created content in commit

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,39 @@
 </script>
 
 <template>
+<br>
+  <div class="container">
+    <div class="card center">
+      <h2>Your IP Address Info</h2>
+      <div class="flex card">
+        <div class="box">
+          <p>Hello</p>
+          <br>
+          <span>111</span>
+        </div>
+        <div class="box">
+          <p>Hello</p>
+          <br>
+          <span>111</span>
+        </div>
+        <div class="box">
+          <p>Hello</p>
+          <br>
+          <span>111</span>
+        </div>
+        <div class="box">
+          <p>Hello</p>
+          <br>
+          <span>111</span>
+        </div>
+        <div class="box">
+          <p>Hello</p>
+          <br>
+          <span>111</span>
+        </div>
+      </div>
+    </div>
+  </div>
 
 </template>
 
